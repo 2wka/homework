e4c2c2cb7ad63b01bf6d6ae0a7ed811b6102b5bb
2wka <168364578+2wka@users.noreply.github.com>
2024-05-01 17:05:02 +0300

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,7 @@
+immutable_var = ( 1,2,34,'apple', True)
+print(immutable_var)
+# строка для замены элемента в кортеже
+# immutable_var[0] = 2002 (не получается выполнить эту часть кода т.к. кортеж является не изменяемым!
+mutable_list = [14,18,85]
+mutable_list[1]= 35
+print(mutable_list)

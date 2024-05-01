ca1b0374bdb68b6b3e711ebabce7f3e161627b90
2wka <168364578+2wka@users.noreply.github.com>
2024-05-01 18:15:32 +0300

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,11 @@
+my_list = ['apple' , 'banana', 'peach','lime','nut','cucumber']
+print(my_list)
+print('Последнее слово: '+ my_list[-1] , 'Первое слово: '+ my_list[0] )
+print(my_list[2:5])
+my_list[2] = ("kivi")
+print(my_list)
+my_dict= {'dir': 9163213211,'Anton': 8800553535,'Nadin': 89501235454}
+print(my_dict)
+print(my_dict['Anton'])
+my_dict['Adre']= 89213211515
+print(my_dict)

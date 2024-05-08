f325bc42a8b1606e6c1955c60e9da482220f649c
2wka <168364578+2wka@users.noreply.github.com>
2024-05-08 19:59:58 +0300

    Add files via upload

---
+++ if.py
@@ -0,0 +1,30 @@
+a, b = 5, 12
+if a < b :
+    print('Hi')
+x = 4
+if x > 0 :
+    print('Дратути!')
+print('Дотвиданья')
+
+if '1234' > '123' :
+    print('work')
+
+if '5' != 5 :
+    print('Good')
+
+#if 1234 > '123' :
+#    print('Привет!')
+#if 112 > [112, 221] :
+#    print('run')
+if [3, 4] > [2, 4] :
+    print('Молодец')
+if '123' < '1234' :
+    print('Молодец')
+if 4 < a < b :
+    print('Успех')
+if 20 > a or b > 13 :
+    print('Успех')
+if 20 > a and b > 13 :
+    print('good')
+if 20 > a and (13 < b or b < 13) :
+    print('Супер!')
